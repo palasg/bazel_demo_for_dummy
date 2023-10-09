03d35f65e0b0396bddd72a3c7de286c918db7b03
palasg <palas1991@gmail.com>
2023-10-10 00:02:02 +0530

    refined

--- lib/srcs/get_time.cpp
+++ lib/srcs/get_time.cpp
@@ -5,8 +5,13 @@
 
 
 char* GetTimeNow(){
+    //current time at system clock
     const auto clock_now = std::chrono::high_resolution_clock::now();
+
+    //chrono time point type needs to convert to std::time_t since ctime is crazy
     std::time_t time_now = std::chrono::system_clock::to_time_t(clock_now);
+    
+    //convert time point to data time
     return std::ctime(&time_now);
 }
 
